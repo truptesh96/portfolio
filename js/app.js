92a69307e9e6e7e8d2dbd18c21ba10e2186479c2
Patel Truptesh <55374686+truptesh96@users.noreply.github.com>
2021-12-05 16:40:13 +0530

    update

--- js/app.js
+++ js/app.js
@@ -1,29 +1,33 @@
 jQuery(function($){
 
-	$.fn.isInViewport = function() {
-	    var elementTop = $(this).offset().top + 100;
-	    var elementBottom = elementTop + $(this).outerHeight();
-	    var viewportTop = $(window).scrollTop();
-	    var viewportBottom = viewportTop + $(window).height();
-	    return elementBottom > viewportTop && elementTop < viewportBottom;
-	};
-
-	
-	function inView(){
-		$(".anim, .wave").each(function(){
-			$(this).isInViewport() ? $(this).addClass("animate") : $(this).removeClass("animate")
-		}
-	)};
-
+	/*------- Custom Animation ------*/
+    function finalAnim(objInstance, delay){
+        var objInstance = objInstance;
+        var timedelay = delay;
+        setTimeout(function(){
+            $(objInstance).addClass("animate");    
+        },timedelay);
+    }
+    $.fn.isInViewport=function(){var t=$(this).offset().top+100,i=t+$(this).outerHeight(),o=$(window).scrollTop(),n=o+$(window).height();return i>o&&t<n};
+    function smoothScroll(){
+        $(".anim:not(.animate)").each(function(i){
+            if( $(this).isInViewport() ) {
+              if($(this).attr("anim-delay") > 0) {
+                 finalAnim($(this), $(this).attr('anim-delay')); 
+              } else {
+                 finalAnim($(this, 0));
+              }
+            }               
+        })}
+    /*------- Custom Animation Ends ------*/
 
 	$(document).ready(function(){
-
-
+		smoothScroll();
 	});
 
 
-	$(window).on('resize scroll onload load', function(){
-		inView();
+	$(window).on('scroll', function(){
+		smoothScroll();
 	});
 
 
