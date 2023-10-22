ec907e416b2dd03a1d9c1d39d0218714d70fcd39
Truptesh Patel <trupteshp@e2msol.com>
2023-10-22 17:58:00 +0530

    up

--- js/app.js
+++ js/app.js
@@ -21,6 +21,30 @@ jQuery(function($){
         })}
     /*------- Custom Animation Ends ------*/
 
+
+    var controller = new ScrollMagic.Controller();
+
+    new ScrollMagic.Scene({triggerElement: ".page-head", triggerHook: 0.5, duration: "40%"})
+    .setTween("body", { background:'#1B0F2D', ease: Linear.easeNone })
+    .addTo(controller);
+
+    new ScrollMagic.Scene({triggerElement: ".page-head .dev", triggerHook: 0.4, duration: "150%"})
+    .setTween(".page-head .dev", {  x:'-60%', ease: Linear.easeNone })
+    .addTo(controller);
+
+    new ScrollMagic.Scene({triggerElement: ".page-head .dev2", triggerHook: 0.5, duration: "150%"})
+    .setTween(".page-head .dev2", {  x:'60%', ease: Linear.easeNone })
+    .addTo(controller);
+
+    new ScrollMagic.Scene({triggerElement: ".green-bg", triggerHook: 0.5, duration: "40%"})
+    .setTween("body", {  background:'rgba(15, 168, 150, .12)', ease: Linear.easeNone })
+    .addTo(controller);
+
+    new ScrollMagic.Scene({triggerElement: ".personal ", triggerHook: 0.5, duration: "80%"})
+    .setTween("body", { background:'rgba(255, 255, 255, .25)', ease: Linear.easeNone })
+    .addTo(controller);     
+
+
 	$(document).ready(function(){
 		smoothScroll();
 	});
